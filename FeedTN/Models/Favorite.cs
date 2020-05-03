130a2a952c326f1a4d3b1cd1a4781f776e46b100
jansenv <jansenv44@gmail.com>
2020-05-03 15:17:56 -0500

    all models built

---
+++ FeedTN/Models/Favorite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedTN.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int FavoriteId { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public int MenuItemId { get; set; }
+
+        public MenuItem MenuItem { get; set; }
+    }
+}
---
+++ FeedTN/Models/Report.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedTN.Models
+{
+    public class Report
+    {
+        [Key]
+        public int ReportId { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Active { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
