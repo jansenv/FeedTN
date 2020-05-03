415bb11b4330538ec5229d1032a373ff412d2105
jansenv <jansenv44@gmail.com>
2020-05-03 14:03:17 -0500

    built Order model

--- FeedTN/Models/Order.cs
+++ FeedTN/Models/Order.cs
@@ -1,6 +1,30 @@
-﻿namespace FeedTN.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FeedTN.Models
 {
     public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateCompleted { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        
+        [Required]
+        public ApplicationUser User { get; set; }
+        public string DriverId { get; set; }
+        public Driver Driver { get; set; }
+        public virtual ICollection<UserMenuItem> UserMenuItems { get; set; }
     }
 }
