3d8e7535d95698a2aa220c35c3c6e128c4ba42fa
jansenv <jansenv44@gmail.com>
2020-05-06 18:40:24 -0500

    can send report

---
+++ FeedTN/Controllers/ReportsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeedTN.Data;
+using FeedTN.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedTN.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ReportsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reports/Create
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: Reports/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Report report)
+        {
+            try
+            {
+                var user = await GetCurrentUserAsync();
+
+                var reportItem = new Report
+                {
+                    Description = report.Description,
+                    Active = true,
+                    UserId = user.Id
+                };
+
+                _context.Report.Add(reportItem);
+                await _context.SaveChangesAsync();
+
+                TempData["reportConfirmation"] = "Report sent";
+
+                return RedirectToAction("Index", "Orders");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
---
+++ FeedTN/Models/ReportViewModels/CreateReportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedTN.Models.ReportViewModels
+{
+    public class CreateReportViewModel
+    {
+        public Report Report { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
