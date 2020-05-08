bc592a22d056de27e53f24f979219beb99248868
jansenv <jansenv44@gmail.com>
2020-05-08 08:00:57 -0500

    text on startup

--- FeedTN/Program.cs
+++ FeedTN/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace FeedTN
 {
@@ -13,6 +15,21 @@ namespace FeedTN
     {
         public static void Main(string[] args)
         {
+            // Find your Account Sid and Token at twilio.com/console
+            // DANGER! This is insecure. See http://twil.io/secure
+            const string accountSid = "AC1c26a9a684895b4cf0a1104847f0b837";
+            const string authToken = "b8733362f602a40fc5ba9c8a959fe184";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                from: new Twilio.Types.PhoneNumber("+12055396681"),
+                to: new Twilio.Types.PhoneNumber("+16156131979")
+            );
+
+            Console.WriteLine(message.Sid);
+
             CreateHostBuilder(args).Build().Run();
         }
 
